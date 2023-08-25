fb4731c9b222376a5dc7fab9e8e754e64e731aae
IvanIsak2000 <vingving648@gmail.com>
2023-08-25 22:18:25 +0300

    remove `(`, fix by pep8

--- database/add.py
+++ database/add.py
@@ -4,6 +4,5 @@ from sqlite3 import connect
 def add_data(user_service: str, user_amount: str):
     with connect("database.db") as db:
         cursor = db.cursor()
-        cursor.execute(("""INSERT INTO service(name,amount) VALUES(?,?)"""),(user_service,user_amount))
+        cursor.execute("""INSERT INTO service(name,amount) VALUES(?,?)""", (user_service, user_amount))
         print("DATA IS UPDATED!")
-
