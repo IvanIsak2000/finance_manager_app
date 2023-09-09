ccefbae0884af9008ad86c96553f6235a29a6ea7
IvanIsak2000 <vingving648@gmail.com>
2023-09-09 23:30:30 +0300

    create separate logger

---
+++ src/logger.py
@@ -0,0 +1,9 @@
+import logging
+import datetime
+
+data = datetime.datetime.today().ctime().split()
+filename = f'{data[1]}_{data[2]}_{"_".join(data[3].split(":"))}.log'
+logging.basicConfig(filename=filename, level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s'
+                    )
+logger = logging.getLogger(__name__)
