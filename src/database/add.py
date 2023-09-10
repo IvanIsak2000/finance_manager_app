e7d26836347b62493aafb504bb31b4346e29f0fc
IvanIsak2000 <vingving648@gmail.com>
2023-09-10 19:40:35 +0300

    add except blocks

--- src/database/add.py
+++ src/database/add.py
@@ -1,11 +1,25 @@
 from database.class_template import *
+from sqlalchemy import exc
+from logger import *
+
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
 
-def add_data(user_service: str, user_amount: str):
-    data = Table(user_service, user_amount)
-    session.add(data)
-    session.commit()
-    print("DATA IS UPDATED!")
+def add_data(user_service: str, user_amount: str) -> str:
+    try:
+        data = Table(user_service, user_amount)
+        session.add(data)
+        session.commit()
+        logger.info(f'Added <{user_service}> <{user_amount}>')
+        return "Successful! Data updated."
+    except exc.IntegrityError:
+        session.rollback()
+        logger.error('Exist data')
+        return 'Error! This data already exists.'
+
+    except Exception as e:
+        session.rollback()
+        logger.exception(e)
+        return e
