7992ff6152dc2e23b781ee2667a7c4609f6aa9f1
IvanIsak2000 <vingving648@gmail.com>
2023-09-10 19:41:17 +0300

    change log format

--- src/logger.py
+++ src/logger.py
@@ -4,6 +4,6 @@ import datetime
 data = datetime.datetime.today().ctime().split()
 filename = f'{data[1]}_{data[2]}_{"_".join(data[3].split(":"))}.log'
 logging.basicConfig(filename=filename, level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(name)s %(message)s'
+                    format='%(asctime)s %(levelname)s %(name)s {%(pathname)s:%(lineno)d} %(message)s'
                     )
 logger = logging.getLogger(__name__)
