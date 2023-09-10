49632f8025526a81cbcdf7ac73648a5244545eaf
IvanIsak2000 <vingving648@gmail.com>
2023-09-10 19:40:54 +0300

    fix imports, add `logger`

--- src/database/delete.py
+++ src/database/delete.py
@@ -1,16 +1,15 @@
 from sqlite3 import connect
-import sys
-from time import sleep
+from logger import *
 
-
-def delete_element(name_to_delete: str):
+def delete_element(name_to_delete: str) -> None:
     with connect('database.db') as db:
         cursor = db.cursor()
         query = """DELETE FROM service WHERE name = ?;"""
         cursor.execute(query, (name_to_delete,))
     delete_success_message = (f'The element {name_to_delete} was'
                               'deleted if it was in the database!')
-    print(delete_success_message)
+    logger.info(f'Element {name_to_delete} was removed if it exist')
+    return print(delete_success_message)
 
 
 def delete_all():
@@ -18,7 +17,6 @@ def delete_all():
         cursor = db.cursor()
         query = """DROP table service;"""
         cursor.execute(query)
+        logger.info('Deleted all data')
         print("All data will be deleted!")
-        print("The program will be terminated")
-    sleep(3)
-    sys.exit()
+        return print("Please reboot program!")
