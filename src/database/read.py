644d92d3731bb5d6309d62f6b836372efcf2307e
IvanIsak2000 <vingving648@gmail.com>
2023-11-09 13:29:46 +0300

    fix issue

--- src/database/read.py
+++ src/database/read.py
@@ -1,12 +1,23 @@
 from database.class_template import *
+import matplotlib.pyplot as plt 
+import matplotlib
 
+matplotlib.use('TkAgg')
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
 
 def read_data():
-    ## Создать отображение графика расходов.
-
+    items = {}
     for i in session.query(Table).all():
-        print(i.name, i.amount)
+        items[i.name] = i.amount
+    names = list(items.keys())
+    amounts = list(items.values())
+    limit = max(amounts) + (max(amounts)/10)
+
+    fig, ax = plt.subplots()
+    bar_container = ax.bar(names, amounts)
+    ax.set(ylabel='Amounts ($)', title='Names', ylim=(0, limit))
+    ax.bar_label(bar_container, fmt='{:,.0f}')
+    plt.show()
