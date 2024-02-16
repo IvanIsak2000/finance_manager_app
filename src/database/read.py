ee0896715f5d83c3d3d93c3bbe1485b76a02b29f
IvanIsak2000 <vingving648@gmail.com>
2024-02-17 00:54:28 +0300

    move to sqlalchemy

--- src/database/read.py
+++ src/database/read.py
@@ -1,23 +1,17 @@
-from database.class_template import *
-import matplotlib.pyplot as plt 
-import matplotlib
+from database.model import *
+import logging 
 
-matplotlib.use('TkAgg')
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
+logger = logging.getLogger(__name__)
+logger.info('Read module start')
 
 def read_data():
-    items = {}
-    for i in session.query(Table).all():
-        items[i.name] = i.amount
-    names = list(items.keys())
-    amounts = list(items.values())
-    limit = max(amounts) + (max(amounts)/10)
+    with Session(engine) as session:
+        logger.info('Connected to db')
+        items = {}
+        for i in session.query(Waste).all():
+            items[i.name] = i.amount
+        names = list(items.keys())
+        amounts = list(items.values())
+        logger.info('Data from database: <%s> <%s>', names, amounts)
+        return names, amounts
 
-    fig, ax = plt.subplots()
-    bar_container = ax.bar(names, amounts)
-    ax.set(ylabel='Amounts ($)', title='Names', ylim=(0, limit))
-    ax.bar_label(bar_container, fmt='{:,.0f}')
-    plt.show()
